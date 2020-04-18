d0454164c4f5ac727d488b028dd4be0b0ca6380a
Ruslan Akhmatdinov <infar45@gmail.com>
2020-04-18 10:35:12 +0300

    Start of the project. Done tilemap pallet and player basic animation and movements

---
+++ Assets/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    //Config
+    [SerializeField] float movingSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    //Cashe
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Run();
+        Jump();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * movingSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
+    }
+
+    private void Jump()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidBody.velocity.x), 1f);
+
+        }
+    }
+}
